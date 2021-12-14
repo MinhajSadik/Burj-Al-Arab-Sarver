0f9f7f1f565f5f5f901b4f356b2b0d2cbeaf5dcc
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-14 19:23:52 +0600

    [ADD] start working

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors');
-const express = require('express')
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const port = 5000
-const pass = 'MongoDbBurjAlArab';
-
-const uri = "mongodb+srv://burjAlArab:MongoDbBurjAlArab@cluster0.iojzl.mongodb.net/burjAlArab?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const PORT = process.env.PORT || 3100;
 
 app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const DB_URI = process.env.DB_URI || "mongodb://localhost:27017";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const client = new MongoClient(DB_URI, options);
 
-client.connect(err => {
-    const bookings = client.db("burjAlArab").collection("booking");
-
-    app.post('/addBooking', (req, res) => {
-        const newBooking = req.body;
-        bookings.insertOne(newBooking)
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-        console.log(newBooking)
-    })
-
-    app.get('/booking', (req, res) => {
-        // console.log(req.query.email)
-        bookings.find({ email: req.query.email })
-            .toArray((err, document) => {
-                res.send(document);
-        })
-    })
-
-
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database Connected...");
+  }
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+const bookings = client.db("burjAlArab").collection("booking");
+
+app.post("/addBooking", (req, res) => {
+  const newBooking = req.body;
+  bookings.insertOne(newBooking).then((result) => {
+    res.send(result.insertedCount > 0);
+  });
+  console.log(newBooking);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/booking", (req, res) => {
+  // console.log(req.query.email)
+  bookings.find({ email: req.query.email }).toArray((err, document) => {
+    res.send(document);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
