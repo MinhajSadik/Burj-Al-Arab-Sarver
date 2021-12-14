3bc2618bfc56ae73a8c4c788c4309a7210ab1996
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-14 22:20:03 +0600

    [ADD]-backend- veryfication done with jwt and firebase-admin

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const { MongoClient } = require("mongodb");
-const cors = require("cors");
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+var cors = require("cors"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  { MongoClient } = require("mongodb"),
+  admin = require("firebase-admin"),
+  serviceAccount = require("./configs/burjAlArab.json"),
+  dotenv = require("dotenv"),
+  app = express();
+
+// dotenv.config();
 
 const PORT = process.env.PORT || 3100;
 
@@ -11,16 +16,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL,
+});
+
 const DB_URI = process.env.DB_URI || "mongodb://localhost:27017";
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 const client = new MongoClient(DB_URI, options);
 
 client.connect((err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log("Database Connected...");
+    console.log("database connected...");
   }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const bookings = client.db("burjAlArab").collection("booking");
@@ -33,17 +47,38 @@ app.post("/addBooking", (req, res) => {
   console.log(newBooking);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/booking", (req, res) => {
-  // console.log(req.query.email)
-  bookings.find({ email: req.query.email }).toArray((err, document) => {
-    res.send(document);
-  });
+  const bearer = req.headers.authorization;
+  if (bearer && bearer.startsWith("Bearer ")) {
+    const idToken = bearer.split(" ")[1];
+    console.log("Bearer:" + bearer);
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        const tokenEmail = decodedToken.email;
+        const queryEmail = req.query.email;
+        if (tokenEmail && queryEmail) {
+          bookings.find({ email: req.query.email }).toArray((err, docs) => {
+            res.send(docs);
+          });
+        } else {
+          res.status(401).send("unAuthorized Access");
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.status(401).send("unAuthorized Access");
+        }
+      });
+  } else {
+    res.status(401).send("unAuthorized Access");
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`server listening on port ${PORT}`);
 });
